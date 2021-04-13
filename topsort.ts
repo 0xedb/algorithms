194b8c469d06d1449ed297deba7d5c68c6ab587e
theBashShell <edohbruno@gmail.com>
2021-04-13 21:05:00 +0000

    top sort extra eample

--- topsort.ts
+++ topsort.ts
@@ -2,20 +2,25 @@ class Point {
   constructor(public name: string, public children: Point[]) {}
 }
 
-const a: Point = new Point("A", []);
-const b: Point = new Point("B", []);
-const c: Point = new Point("C", []);
-const d: Point = new Point("D", []);
-const e: Point = new Point("E", []);
-const f: Point = new Point("F", []);
+const a: Point = new Point("shirt", []);
+const b: Point = new Point("hoodie", []);
+const c: Point = new Point("underwear", []);
+const d: Point = new Point("pants", []);
+const e: Point = new Point("school", []);
+const f: Point = new Point("socks", []);
+const g: Point = new Point("shoes", []);
+
 
 a.children.push(b);
-b.children.push(c, d, e);
-d.children.push(e);
-e.children.push(f);
+b.children.push(e);
+c.children.push(d)
+d.children.push(g);
+f.children.push(g)
+
+// e.children.push(f);
 
 
-const nodes = new Array(a,b,c,d,e,f)
+const nodes = new Array(a,b,c,d,e,f, g)
 const visited = new WeakSet()
 const stk = new Array()
 
